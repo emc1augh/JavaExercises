ac75ba73b1496608d962cc43d92f2f3998e8a695
Ed McLaughlin <emclaughlin099@gmail.com>
2018-01-24 15:59:52 +0000

    Start of Tutorial 2

--- src/Tutorial2/Booleans.java
+++ src/Tutorial2/Booleans.java
@@ -1,4 +1,28 @@
 package Tutorial2;
 
+import java.util.Scanner;
+
 public class Booleans {
+
+    private static boolean isEven (int number) {
+        boolean isEven = (number % 2 == 0);
+
+            return isEven;
+    }
+
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number or type 'exit' to terminate the system");
+        while (scanner.hasNextInt()){
+            int eventest = scanner.nextInt();
+            if(isEven(eventest)){
+            System.out.println("This number is even");
+            } else {
+            System.out.println("This number is odd");
+            }
+            if (scanner.hasNext("exit")){
+                System.exit(0);
+            }
+        }
+    }
 }
